78e23ebefe896af5d8b476381dc356c366737607
Michael Lancaster <michaell.llancaster@gmail.com>
2018-02-03 05:31:06 -0800

    Check for Content-Type and bug fixes
    Improve scrapper substantially checking for Content-Type HTML and fix a couple cases when trying to scrap formats like pdf, png, jpg.
    Fix docker-compose issue adding host
    General code refactoring/improvements.

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ from bs4 import BeautifulSoup as soup
 
 app = Flask(__name__)
 
+MESSAGES = {
+    "INVALID_URL": "Not able to automatically parse the url.",
+    "SOMETHING_WRONG": "Sorry but something went wrong and the request wasn't able to complete."
+}
+
 @app.route("/scrap", methods=["POST"])
 def scrap_url():
     """
@@ -20,32 +25,106 @@ def scrap_url():
 
     if pageContent == None:
         return jsonify({
-            "message": "Not able to parse the url"
+            "message": MESSAGES["SOMETHING_WRONG"],
+            "data": {}
         }), 400
 
+    if isinstance(pageContent, dict):
+        invalidContent = get_invalid_content(url=url, contentType=pageContent["Content-Type"])
+
+        return jsonify({
+            "message": MESSAGES["INVALID_URL"],
+            "data": invalidContent
+        }), pageContent["code"]
+
     parsedHTML = soup(pageContent, "html.parser")
-    formattedContent = format_content(parsedHTML, url)
+    validContent = get_valid_content(parsedHTML, url)
 
     return jsonify({
-        "data": formattedContent
+        "data": validContent
     }), 200
 
 def request_url(url):
+    """Get url content and check if it returns 200
+    if so check if Content-Type is HTML otherwise throw
+    
+    Arguments:
+        url {String} -- url domain
+    
+    Returns:
+        [String] -- if 200 and Content-Type HTML successful returns content
+        [Int] -- if 200 but Content-Type NOT HTML returns status code 206
+        [None] -- if NOT 200 or exception returns None
     """
-    Request url and return the markup
-    """
+
     try:
-        page = requests.get(url, timeout=5)
-        if page.status_code == 200:
-            return page.content
-    except requests.exceptions.Timeout:
-        return None
-    except requests.exceptions.TooManyRedirects:
-        return None
-    except requests.exceptions.RequestException as error:
+        response = requests.get(url, timeout=3)
+        
+        if response.status_code == 200:
+            contentType = response.headers["Content-Type"]
+            isValidDocType = is_valid_doc_type(contentType)
+            
+            if isValidDocType is not True:
+                return {
+                    "code": 206,
+                    "Content-Type": contentType
+                }
+
+            return response.content
+
+    except (
+        requests.exceptions.InvalidURL,
+        requests.exceptions.ContentDecodingError,
+        requests.exceptions.Timeout, 
+        requests.exceptions.TooManyRedirects,
+        requests.exceptions.RequestException):
         return None
 
-def format_content(content, url):
+def is_valid_doc_type(contentType):
+    """Check if page content is other than HTML doctype
+    
+    Arguments:
+        contentType {String} -- page Content-Type (pdf/json/html/mp3/etc)
+    
+    Returns:
+        [Boolen] -- Returns True if doctype is html otherwise False
+    """
+    if "text/html" not in contentType:
+        return False
+
+    return True
+
+def get_invalid_content(url, contentType):
+    """Returns expected collection format but only with url
+    
+    Arguments:
+        url {String} -- url domain
+        contentType {String} -- request content-type
+    
+    Returns:
+        [Dict] -- collection format
+    """
+    ACCEPTED_THUMBNAIL = [
+        "image/jpeg",
+        "image/png",
+        "image/webp"
+    ]
+    thumbnail = ""
+
+    if any(type in contentType for type in ACCEPTED_THUMBNAIL):
+        thumbnail = url
+    
+
+    collection = {
+        "title": "Untitled - EDIT ME",
+        "description": "",
+        "url": url,
+        "thumbnail": thumbnail
+    }
+
+    return collection
+
+def get_valid_content(content, url):
     """
     Get content from metas and return object formated
     """
@@ -79,6 +158,10 @@ def get_meta_content(valueA, valueB):
     return valueA
 
 if __name__ == "__main__":
+    HOST = "0.0.0.0"
+    PORT = os.getenv("PORT", 5000)
+
     app.run(
-        port=os.getenv("PORT", 5500)
+        host=HOST,
+        port=PORT
     )
